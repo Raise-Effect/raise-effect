6300156c2fbc43eff828e2e22b2c06168bc4a38b
Jaron Heard <jaronheard@gmail.com>
2015-09-20 18:56:16 -0700

    added slider view. no syntax errors but slider view does not show on main page

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -3,6 +3,7 @@ import Backbone from 'backbone';
 // Views
 import RootView from '../views/root';
 import MapView from  '../views/main/map'
+import SliderView from '../views/main/slider'
 import Homepage from '../views/main/homepage';
 
 let AppController = Backbone.View.extend({
@@ -15,9 +16,11 @@ let AppController = Backbone.View.extend({
     index() {
         let homepage = new Homepage();
         let map      = new MapView();
+				let slider   = new SliderView();
 
         this.rootView.setView(homepage, {trigger: true});
         this.rootView.addMap(map, {trigger: true});
+				this.rootView.addSlider(slider, {trigger: true});
     }
 });
 

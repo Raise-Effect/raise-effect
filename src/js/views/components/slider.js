8d78cc6d978ac690bc3bf77f2a4c6aa13cfefc0b
Jaron Heard <jaronheard@gmail.com>
2015-10-02 22:16:54 -0700

    slider working, decomposed slider.js to slider.js and sliderTooltip.js

--- src/js/views/components/slider.js
+++ src/js/views/components/slider.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import ReactSlider from 'rc-slider';
-
-let SliderTooltip = React.createClass({
-	render() {
-		return (
-			<div class="panel panel-default">
-	            <div class="panel-heading">
-	              <h3 class="panel-title">Wage Breakdown</h3>
-	            </div>
-	            <div class="panel-body">
-	              <ul class="list-inline">
-	                <li>$<span class="hour">{ this.props.value }</span> / hour</li>
-	                <li>$<span class="day">{ this.props.value * 8 }</span> / day</li>
-	                <li>$<span class="week">{ this.props.value * 8 * 5 }</span> / week</li>
-	                <li>$<span class="month">{ this.props.value * 8 * 5 * 4 }</span> / month</li>
-	                <li>$<span class="year">{ this.props.value * 8 * 5 * 4 * 12 }</span> / year</li>
-	              </ul>
-	            </div>
-          	</div>
-		);
-	}
-});
+import SliderTooltip from '../components/sliderTooltip';
 
 let Slider = React.createClass({
 	getInitialState() {
--- src/js/views/components/sliderTooltip.js
+++ src/js/views/components/sliderTooltip.js
@@ -2,24 +2,24 @@ import React from 'react';
 import _ from 'lodash';
 
 let SliderTooltip = React.createClass({
-	render() {
-		return (
-					<div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title">Wage Breakdown</h3>
+  render() {
+    return (
+      <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">Wage Breakdown</h3>
+              </div>
+              <div class="panel-body">
+                <ul class="list-inline">
+                  <li>$<span class="hour">{ this.props.value }</span> / hour</li>
+                  <li>$<span class="day">{ this.props.value * 8 }</span> / day</li>
+                  <li>$<span class="week">{ this.props.value * 8 * 5 }</span> / week</li>
+                  <li>$<span class="month">{ this.props.value * 8 * 5 * 4 }</span> / month</li>
+                  <li>$<span class="year">{ this.props.value * 8 * 5 * 4 * 12 }</span> / year</li>
+                </ul>
+              </div>
             </div>
-            <div class="panel-body">
-              <ul class="list-inline">
-                <li>$<span class="hour">{ this.state.value }</span> / hour</li>
-                <li>$<span class="day">{ 13.5 * 8 }</span> / day</li>
-                <li>$<span class="week">{ 13.5 * 8 * 5 }</span> / week</li>
-                <li>$<span class="month">{ 13.5 * 8 * 5 * 4 }</span> / month</li>
-                <li>$<span class="year">{ 13.5 * 8 * 5 * 4 * 12 }</span> / year</li>
-              </ul>
-            </div>
-          </div>
-		);
-	}
+    );
+  }
 });
 
 export default SliderTooltip;

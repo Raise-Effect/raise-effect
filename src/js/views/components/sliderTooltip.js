a2580f27234c5d922528bda14cb27ba643e1500d
Nick Bernard <nick@nrbernard.com>
2015-10-04 12:44:37 -0700

    Add Bootstrap class names

--- src/js/views/components/sliderTooltip.js
+++ src/js/views/components/sliderTooltip.js
@@ -4,20 +4,20 @@ import _ from 'lodash';
 let SliderTooltip = React.createClass({
   render() {
     return (
-      <div class="panel panel-default">
-              <div class="panel-heading">
-                <h3 class="panel-title">Wage Breakdown</h3>
-              </div>
-              <div class="panel-body">
-                <ul class="list-inline">
-                  <li>$<span class="hour">{ this.props.value }</span> / hour</li>
-                  <li>$<span class="day">{ this.props.value * 8 }</span> / day</li>
-                  <li>$<span class="week">{ this.props.value * 8 * 5 }</span> / week</li>
-                  <li>$<span class="month">{ this.props.value * 8 * 5 * 4 }</span> / month</li>
-                  <li>$<span class="year">{ this.props.value * 8 * 5 * 4 * 12 }</span> / year</li>
-                </ul>
-              </div>
-            </div>
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title">Wage Breakdown</h3>
+        </div>
+        <div className="panel-body">
+          <ul className="list-inline">
+            <li>$<span className="hour">{ this.props.value }</span> / hour</li>
+            <li>$<span className="day">{ this.props.value * 8 }</span> / day</li>
+            <li>$<span className="week">{ this.props.value * 8 * 5 }</span> / week</li>
+            <li>$<span className="month">{ this.props.value * 8 * 5 * 4 }</span> / month</li>
+            <li>$<span className="year">{ this.props.value * 8 * 5 * 4 * 12 }</span> / year</li>
+          </ul>
+        </div>
+      </div>
     );
   }
 });

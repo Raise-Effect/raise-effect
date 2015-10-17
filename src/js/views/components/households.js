445c56edfb0cf219d833b18b535dbaee6ac53047
Daniel West <daniel.j.west@gmail.com>
2015-10-17 11:25:04 -0400

    Removing loads where not required and cleaning up the progressBar a bit.

--- src/js/views/components/households.js
+++ src/js/views/components/households.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import _ from 'lodash';
 import FamilyTypeBreakdown from '../components/familyTypeBreakdown';
 
 
--- src/js/views/components/progressBar.js
+++ src/js/views/components/progressBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import _ from 'lodash';
-
 
 let ProgressBar = React.createClass({
-	render() {
+   propTypes: {
+     completed: React.PropTypes.number.isRequired,
+   },
+   getDefaultProps: function() {
+     return {
+       completed: 0
+     };
+   },
+	 render: function() {
+    let completed = this.props.completed;
+    if (completed < 0) {completed = 0};
+    if (completed > 100) {completed = 100};
 		return (
             <div className="progress">
                 <div className="progress-bar" role="progressbar" aria-valuenow={this.props.completed} style={{width:this.props.completed + "%"}} aria-valuemin="0" aria-valuemax="100">

738f96d1757b3ee4d21df68a3a96b1a51554e2f0
jaronheard <jaronheard@gmail.com>
2015-10-22 13:16:23 -0700

    set tooltip weekly, monthly, annual wages per study

--- src/js/views/components/sliderTooltip.js
+++ src/js/views/components/sliderTooltip.js
@@ -12,9 +12,9 @@ let SliderTooltip = React.createClass({
           <ul className="list-inline">
             <li>$<span className="hour">{ this.props.value }</span> / hour</li>
             <li>$<span className="day">{ this.props.value * 8 }</span> / day</li>
-            <li>$<span className="week">{ this.props.value * 8 * 5 }</span> / week</li>
-            <li>$<span className="month">{ this.props.value * 8 * 5 * 4 }</span> / month</li>
-            <li>$<span className="year">{ this.props.value * 8 * 5 * 4 * 12 }</span> / year</li>
+            <li>$<span className="week">{ this.props.value * 8 * 22 * 12 / 52 }</span> / week</li>
+            <li>$<span className="month">{ this.props.value * 8 * 22 }</span> / month</li>
+            <li>$<span className="year">{ this.props.value * 8 * 22 * 12 }</span> / year</li>
           </ul>
         </div>
       </div>

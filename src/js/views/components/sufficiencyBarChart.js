b4261a098c73f65493ae41cc3d4b43f5d9b927e2
Nick Bernard <nick@nrbernard.com>
2015-10-24 11:34:58 -0700

    Add Nvd3 charts with sample data

--- src/js/views/components/sufficiencyBarChart.js
+++ src/js/views/components/sufficiencyBarChart.js
@@ -1,47 +1,45 @@
 import React from "react";
 import {findDOMNode} from "react-dom";
 import _ from "lodash";
+import d3 from "d3";
+import nv from "nvd3";
 import SufficiencyBarLine from "../components/sufficiencyBarLine"
 
 let SufficiencyBarChart = React.createClass({
-   propTypes: {
+  getDefaultProps: function() {
+    return { sufficiency: {}, groups: [] };
+  },
 
-   },
-   getDefaultProps: function() {
-     return { sufficiency: {}, groups: [] };
-   },
-   getHeaders: function() {
-     return _.map(this.props.groups, (group, i) => {
-       return (
-         <div key={i} className="horizontal text-right">
-             <span key={group.name}>{group.name}</span>
-         </div>
-       )
-     });
-   },
-   getLines: function() {
-     return _.map(this.props.groups, (group) => {
-       return (
-         <SufficiencyBarLine
-           key={group.populationKey}
-           percentage={this.props.sufficiency[group.populationKey]}
-         />
-       )
-     });
-   },
-	 render: function() {
-		return (
+  getHeaders: function() {
+    return _.map(this.props.groups, (group, i) => {
+      return (
+        <div key={i} className="horizontal text-right">
+          <span key={group.name}>{group.name}</span>
+        </div>
+      )
+    });
+  },
+
+  getBars: function() {
+    return _.map(this.props.groups, (group) => {
+      return (
+        <SufficiencyBarLine
+          key={group.populationKey}
+          familyType={group.populationKey}
+          percentage={this.props.sufficiency[group.populationKey]}
+        />
+      )
+    });
+  },
+
+  render: function() {
+    return (
       <div className="row horizontal rounded">
-           <div className="col-xs-3">
-            {this.getHeaders()}
-          </div>
-        <div className="col-xs-9">
-          <div className="dotted-divider"/>
-            {this.getLines()}
+        <div className="col-xs-12">
+            {this.getBars()}
         </div>
      </div>
-
-		);
+    );
 	}
 });
 
--- src/js/views/components/sufficiencyBarLine.js
+++ src/js/views/components/sufficiencyBarLine.js
@@ -2,37 +2,44 @@ import React from "react";
 import {findDOMNode} from "react-dom";
 import classNames from "classnames";
 
-
 let SufficiencyBarLine = React.createClass({
-   propTypes: {
+  propTypes: {
 
-   },
-   getDefaultProps: function() {
-     return { percentage: 0};
-   },
-	 render: function() {
-     let percent = this.props.percentage;
-     let color = classNames("progress-fill", "under")
-     if (this.props.percentage >= 150)
-     {
-       color = classNames("progress-fill", "over")
-     }
-     else if (this.props.percentage >= 100) {
-       color = classNames("progress-fill", "sufficient")
-     }
-     if (this.props.percentage > 200)
-     {
-       percent = 200;
-     }
-		return (
-      <div className="horizontal">
-        <div className="progress-track">
-          <div className={color} style={{width:percent / 2 + "%"}}>
-            <span>{this.props.percentage}%</span>
-          </div>
-        </div>
-      </div>
-      )
-      }
-      });
+  },
+
+  getDefaultProps: function() {
+    return { percentage: 0};
+  },
+
+  render: function() {
+    return (
+      <div className={this.props.familyType + "-chart"}></div>
+    )
+  },
+
+  componentDidMount: function() {
+    let chart = nv.models.bulletChart();
+
+    let dataWithLabels = [{
+      "title":"Revenue",
+      "subtitle":"US$, in thousands",
+      "ranges":[150,225,300],
+      "measures":[220],
+      "markers":[250, 100],
+      "markerLabels":['Target Inventory', 'Low Inventory'],
+      "rangeLabels":['Maximum Inventory','Average Inventory','Minimum Inventory'],
+      "measureLabels":['Current Inventory']
+    }];
+
+    let chartName = "." + this.props.familyType + "-chart";
+
+    let vis2 = d3.select(chartName).selectAll("svg")
+      .data(dataWithLabels)
+      .enter().append('svg')
+      .attr('class',"bullet nvd3")
+      .transition().duration(1000)
+      .call(chart);
+  }
+});
+
 export default SufficiencyBarLine;

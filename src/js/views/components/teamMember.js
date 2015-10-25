81d04bf2bd87acdf943d4b043e2c2447ea87810d
Daniel West <daniel.j.west@gmail.com>
2015-10-25 11:28:16 -0400

    Fixed a graphical glitch that occurred when the resolution didn't fit into small yet but was too small to fit 4 pictures in a row.

--- src/js/views/components/teamMember.js
+++ src/js/views/components/teamMember.js
@@ -4,7 +4,7 @@ let TeamMember = React.createClass({
 
   render: function() {
     return (
-      <div key={this.props.name} className="member col-sm-4 col-md-3">
+      <div key={this.props.name} className="member col-sm-4 col-md-4 col-lg-3">
         <img className="img-responsive member" src={"./public/images/team/" + this.props.photo } />
         <h2>{this.props.name}</h2>
         <h3>{this.props.title}</h3>
